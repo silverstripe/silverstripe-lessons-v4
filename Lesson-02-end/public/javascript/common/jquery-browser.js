439b3c3a3c3040733d042780382c6934ce4f60bf
Manuel Thalmann <m@nuth.ch>
2019-10-11 01:09:50 +0200

    Copy Lesson-02-begin to Lesson-02-end

--- Lesson-02-end/public/javascript/common/jquery-browser.js
+++
@@ -1,4 +0,0 @@
-/**
- * Check if browser is a mobile device
- */
-(function(){(jQuery.browser=jQuery.browser||{}).mobile=(/android|webos|iphone|ipad|ipod|blackberry/i.test(navigator.userAgent.toLowerCase()));}(navigator.userAgent||navigator.vendor||window.opera));
