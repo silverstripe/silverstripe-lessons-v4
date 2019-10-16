9fa128f4108542278b272ecde06460db5f1ed814
Manuel Thalmann <m@nuth.ch>
2019-10-16 02:30:46 +0200

    Rearrange the `Lesson-08-end` environment

--- Lesson-08-end/app/src/HomePageController.php
+++ Lesson-08-end/app/src/HomePageController.php
@@ -1,16 +1,10 @@
 <?php
 
-namespace SilverStripe\Lessons;
+namespace SilverStripe\Example;
 
 use PageController;    
 
 class HomePageController extends PageController 
 {
-    public function LatestArticles($count = 3)
-    {
-        return ArticlePage::get()
-            ->sort('Created', 'DESC')
-            ->limit($count);
-    }
 
 }
