88b158e09f146726618aaf49d9cc164de4ee34ab
Manuel Thalmann <m@nuth.ch>
2019-10-17 21:11:23 +0200

    Refactor the sql file for setting property dates

--- Lesson-15-begin/__assets/set_property_dates.sql
+++ Lesson-15-begin/__assets/set_property_dates.sql
@@ -1,19 +1,40 @@
-UPDATE SilverStripe_Lessons_Property SET AvailableStart = FROM_UNIXTIME(
+UPDATE SilverStripe_Example_Property SET AvailableStart = FROM_UNIXTIME(
         UNIX_TIMESTAMP(NOW()) + FLOOR(0 + (RAND() * 31536000))
 );
-UPDATE SilverStripe_Lessons_Property SET AvailableEnd = FROM_UNIXTIME(
+UPDATE SilverStripe_Example_Property SET AvailableEnd = FROM_UNIXTIME(
         UNIX_TIMESTAMP(AvailableStart) + FLOOR(1 + (RAND() * 1209600))
 );
 
-UPDATE SilverStripe_Lessons_Property_Live SET AvailableStart = (
-  SELECT AvailableStart
-    FROM SilverStripe_Lessons_Property
-    WHERE
-      SilverStripe_Lessons_Property.ID = SilverStripe_Lessons_Property_Live.ID
-);
-UPDATE SilverStripe_Lessons_Property_Live SET AvailableEnd = (
+UPDATE SilverStripe_Example_Property_Live SET
+  AvailableStart = (
+    SELECT AvailableStart
+      FROM SilverStripe_Example_Property
+     WHERE
+        SilverStripe_Example_Property.ID = SilverStripe_Example_Property_Live.ID
+  ),
+  AvailableEnd = (
   SELECT AvailableEnd
-    FROM SilverStripe_Lessons_Property
-    WHERE
-      SilverStripe_Lessons_Property.ID = SilverStripe_Lessons_Property_Live.ID
-);
+    FROM SilverStripe_Example_Property
+   WHERE
+      SilverStripe_Example_Property.ID = SilverStripe_Example_Property_Live.ID
+  );
+
+UPDATE SilverStripe_Example_Property_Versions SET
+  AvailableStart = (
+    SELECT AvailableStart
+      FROM SilverStripe_Example_Property
+     WHERE
+        SilverStripe_Example_Property.ID = RecordID
+  ),
+  AvailableEnd = (
+  SELECT AvailableEnd
+    FROM SilverStripe_Example_Property
+   WHERE
+      SilverStripe_Example_Property.ID = RecordID
+  )
+WHERE
+  Version = (
+    SELECT MAX(Version)
+      FROM (SELECT  RecordID "Rec", Version FROM SilverStripe_Example_Property_Versions) AS v
+     WHERE Rec = RecordID
+  );
